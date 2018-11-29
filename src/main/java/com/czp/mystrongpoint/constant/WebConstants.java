10713ef462313e25eac9442114e672d4e608649a
陈志平 <chenzhiping@shouqianba.com>
2018-11-29 18:14:59 +0800

    添加拦截器

---
+++ src/main/java/com/czp/mystrongpoint/constant/WebConstants.java
@@ -0,0 +1,8 @@
+package com.czp.mystrongpoint.constant;
+
+public final class WebConstants {
+
+    public static final String WEB_LOGIN_USER = "login_user"; // session中存储的key值
+    public static final String WEB_USER_ID_COOKIE = "id_cookie"; //
+
+}
---
+++ src/main/java/com/czp/mystrongpoint/interceptor/LoginInterceptor.java
@@ -0,0 +1,76 @@
+package com.czp.mystrongpoint.interceptor;
+
+import com.czp.mystrongpoint.constant.WebConstants;
+import com.czp.mystrongpoint.model.Rouge;
+import com.czp.mystrongpoint.service.RougeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginInterceptor.class);
+
+    @Autowired
+    private RougeService rougeService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        String uri = request.getRequestURI();
+        String remoteHost = request.getRemoteHost();
+        log.info("Request url: [{}] from [{}]", uri, remoteHost);
+        HttpSession session = request.getSession();
+        if (Objects.isNull(session)
+                || Objects.isNull(session.getAttribute(WebConstants.WEB_LOGIN_USER))) {
+            // session获取不到用户，看cookie能不能获取到
+            String uid = getCookieId(request);
+            if (Objects.isNull(uid)) {
+                // 如果cookie为空
+                response.sendRedirect(request.getContextPath() + "/login");
+                return false;
+            }
+            // cookieId不为空，用cookieId获取数据库中的用户
+            Rouge rouge = rougeService.getRouge(uid);
+            if (Objects.isNull(rouge)) {
+                // 无效用户
+                response.sendRedirect(request.getContextPath() + "/login");
+                return false;
+            }
+            // 设置session
+            request.getSession().setAttribute(WebConstants.WEB_LOGIN_USER, rouge);
+        }
+
+        Rouge rouge = (Rouge) session.getAttribute(WebConstants.WEB_LOGIN_USER);
+        log.info("The operator: [{}] go towards the uri: [{}]", rouge.getName(), uri);
+
+        return true;
+    }
+
+    private String getCookieId(HttpServletRequest request) {
+        if (!Objects.isNull(request)) {
+            Cookie[] cookies = request.getCookies();
+            if (cookies == null || cookies.length < 1) {
+                return null;
+            }
+            for (Cookie cookie : cookies) {
+                if (WebConstants.WEB_USER_ID_COOKIE.equals(cookie.getName())) {
+                    // TODO 之后cookie要加密
+                    return cookie.getValue();
+                }
+            }
+        }
+
+        return null;
+    }
+
+}
